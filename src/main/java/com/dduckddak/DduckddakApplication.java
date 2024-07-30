820f81150ed9e97eca08544c92aca32c55ca4c98
angelSuho <angelsuho0303@gmail.com>
2024-07-30 19:15:41 +0900

    project init

---
+++ src/main/java/com/dduckddak/DduckddakApplication.java
@@ -0,0 +1,13 @@
+package com.dduckddak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DduckddakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DduckddakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dduckddak/DduckddakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dduckddak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DduckddakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
