6d66771cc4612323650d092124ee3fb44d1af81c
seungnYong <77851079+gkfktkrh153@users.noreply.github.com>
2024-08-06 22:15:39 +0900

    Update WebConfig.java

--- src/main/java/com/dduckddak/global/config/WebConfig.java
+++ src/main/java/com/dduckddak/global/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // 안에 해당 주소를 넣어도 됨
+                        .allowedOrigins("http://localhost:3000", "https://www.gadduck.info") // 안에 해당 주소를 넣어도 됨
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
                         .allowCredentials(true); // .allowedOriginPatterns("*") 이렇게 와일드 카드로 설정하면 이거 쓰면 에러남 ( 실행 조차  X )
