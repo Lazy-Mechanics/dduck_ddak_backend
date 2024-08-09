b0361c19aa2d3c21784fc7d55ea6d09024bef278
gkfktkrh153 <gkfktkrh153@naver.com>
2024-08-09 11:33:41 +0900

    swagger cors

--- src/main/java/com/dduckddak/DduckddakApplication.java
+++ src/main/java/com/dduckddak/DduckddakApplication.java
@@ -1,9 +1,14 @@
 package com.dduckddak;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {
+		@Server(url = "https://api.gadduck.info", description = "Default Server URL")
+})
 public class DduckddakApplication {
 
 	public static void main(String[] args) {
