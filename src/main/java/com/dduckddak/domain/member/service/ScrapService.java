f2e62d16e133ba757dbf0ec478d6a06cbf68789b
SoohoLee <hyeri0603@naver.com>
2024-08-14 00:04:47 +0900

    refactor: 인증 경로 추가 (#noisssue)

--- src/main/java/com/dduckddak/domain/member/service/ScrapService.java
+++ src/main/java/com/dduckddak/domain/member/service/ScrapService.java
@@ -9,6 +9,7 @@ import com.dduckddak.domain.member.repository.ScrapRepository;
 import com.dduckddak.domain.town.model.Town;
 import com.dduckddak.domain.town.repository.TownRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class ScrapService {
     private final TownRepository townRepository;
     private final ScrapRepository scrapRepository;
@@ -27,6 +29,7 @@ public class ScrapService {
         Member member = memberRepository.findByEmail(scrapRequest.getEmail())
                 .orElseThrow(() -> new RuntimeException("해당 사용자가 존재하지 않습니다."));
 
+        log.info("ScrapRequest: {}", scrapRequest);
         Scrap scrap = new Scrap(scrapRequest, member);
         if (scrapRepository.existsByTownCodeAndIndustryNameAndQuarter(scrap.getTownCode(), scrap.getIndustryName(), scrap.getQuarter())) {
             throw new RuntimeException("이미 스크랩한 지역입니다.");
